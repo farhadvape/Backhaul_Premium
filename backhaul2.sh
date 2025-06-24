7c14720665fe3b53e7cec814b8a7e0ca836e7422
Armin <40608213+ArminNy@users.noreply.github.com>
2025-06-24 17:26:29 +0330

    Backhaul Update

--- backhaul2.sh
+++ backhaul2.sh
@@ -183,7 +183,7 @@ EOF
     echo -e "${NC}${GREEN}"
     echo -e "Script Version: ${YELLOW}${SCRIPT_VERSION}${GREEN}"
     if [[ -f "${config_dir}/backhaul_premium" ]]; then
-    	echo -e "Core Version: ${YELLOW}$($config_dir/backhaul_premium -v)${GREEN}"
+    	echo -e "Core Version: v1.1.9"
     fi
     echo -e "Telegram Channel: ${YELLOW}@DELTATM${NC}"
 }
